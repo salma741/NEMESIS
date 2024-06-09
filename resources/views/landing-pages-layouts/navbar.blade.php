67a35f16fb1a49658282b83ca7d3b33231af5609
Put <putriwibowo73@gmail.com>
2024-06-09 20:23:28 +0700

    navbar

--- resources/views/landing-pages-layouts/navbar.blade.php
+++ resources/views/landing-pages-layouts/navbar.blade.php
@@ -5,7 +5,7 @@
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         @foreach ($configurations as $index => $configuration )
-                        <a href="index.html" class="logo">{{ $configuration->name }}<em> Gym</em></a>
+                        <a href="index.html" class="logo me-3">{{ $configuration->name }}<em> Gym</em></a>
                         @endforeach
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
@@ -15,9 +15,6 @@
                             <li class="scroll-to-section"><a href="#our-classes">Supplement</a></li>
                             <li class="scroll-to-section"><a href="#schedule">Membership</a></li>
                             <li class="scroll-to-section"><a href="#contact-us">Contact</a></li>
-                            @if($hasRegistrations)
-                            <li class="scroll-to-section"><a href="{{ URL::to('/registration-member') }}">My Registrations</a></li>
-                            @endif
                             @if(auth()->check())
                             <li class="nav-item dropdown scroll-to-section">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -25,7 +22,9 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('member-profile') }}">Edit Profile</a>
-                                    <a class="dropdown-item" href="#">Your Member Package Registration</a>
+                                    @if($hasRegistrations)
+                                    <a class="dropdown-item" href="{{ URL::to('/registration-member') }}">My Registrations</a>
+                                    @endif
                                     <a class="dropdown-item" href="#">Your Status</a>
                                 </div>
                             </li>
