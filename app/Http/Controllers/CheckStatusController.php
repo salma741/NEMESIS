4b92e285d0766de4aee518b082c391f78ca1a558
Refky <refkimaulana123@gmail.com>
2024-06-11 09:45:16 +0700

    kami mengerjakan check status

--- app/Http/Controllers/CheckStatusController.php
+++ app/Http/Controllers/CheckStatusController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\CheckStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CheckStatusController extends Controller
 {
+    public function index ()
+    {
+
+        $checkstatuss = CheckStatus::all();
+       
+        $data = [
+            'title' => 'check',
+            'checkstatuss' => $checkstatuss,
+        ];
+
+        return view('check-status.index', $data);
+    }
+
+
 }
